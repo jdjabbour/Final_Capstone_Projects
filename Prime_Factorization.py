69fd61af2aeae18fcfd7d3b073a38a4c25b1602b
jdjabbour <jdjabbour@live.com>
2017-08-16 15:40:07 -0400

    Work in Progress

---
+++ Prime_Factorization.py
@@ -0,0 +1,16 @@
+####################################################################################################################
+# Udemy Complete Python Bootcamp
+# Final Capstone Project: Prime Factorization
+# Completed on: 
+####################################################################################################################
+
+def prime_num(n):
+    for i in range(n):
+        if i%2 != 0 and i%3 != 0:
+            print(iter(i))
+        else:
+            pass
+
+print(prime_num(10))
+
+
